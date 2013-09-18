43711dcbcd2d163fbf6263696e449c1fca6cb6e7
fqfpc2003 <ifan0972@qq.com>
2013-09-18 15:02:38 +0800

    Update openstack_nova.sh

--- slaver/openstack_nova.sh
+++ slaver/openstack_nova.sh
@@ -10,23 +10,16 @@ host_ip=$(/sbin/ifconfig eth0| sed -n 's/.*inet *addr:\([0-9\.]*\).*/\1/p')
 echo "#############################################################################################################"
 echo "The IP address for eth0 is probably $host_ip".
 echo "#############################################################################################################"
-read -p "Enter the ServerControlIP interface IP: " host_ip_entry
-read -p "Enter the computeControlIP ethernet interface IP: " host_ip_con
+read -p "Enter the Master Server IP : " host_ip_entry
+read -p "Enter the Localhost ethernet interface IP: " host_ip_local
 read -p "Enter the fixed network (eg. 10.0.2.32/27): " fixed_range
-read -p "Enter the fixed starting IP (eg. 10.0.2.33): " fixed_start
-echo "#######################################################################################"
-echo "The floating range can be a subset of your current network.  Configure your DHCP server"
-echo "to block out the range before you choose it here.  An example would be 10.0.1.224-255"
-echo "#######################################################################################"
-read -p "Enter the floating network (eg. 10.0.1.224/29): " floating_range
-read -p "Enter the floating network size (eg. 32): " floating_size
 read -p "Enter the admin password : " SERVICE_PASSWORD
 
 # get nova
 apt-get update
 apt-get install nova-api nova-cert nova-common nova-objectstore nova-scheduler nova-volume nova-consoleauth novnc python-nova python-novaclient nova-compute nova-compute-kvm  nova-network
 
-./stackrc
+
 password=$SERVICE_PASSWORD
 
 # hack up the nova paste file
@@ -38,50 +31,72 @@ s,%SERVICE_PASSWORD%,$password,g;
  
 # write out a new nova file
 echo "
---dhcpbridge_flagfile=/etc/nova/nova.conf
---dhcpbridge=/usr/bin/nova-dhcpbridge
+# LOGS/STATE
+--verbose=False
+
+# Authentication
+--auth_strategy=keystone
+
+# Ccheduler
+--scheduler_driver=nova.scheduler.simple.SimpleScheduler
+
+# Volumes
+--volume_group=nova-volumes
+--iscsi_helper=tgtadm
+
+# Datebase
+--sql_connection=mysql://nova:$password@$host_ip_entry/nova
+
+# Nova
 --logdir=/var/log/nova
 --state_path=/var/lib/nova
 --lock_path=/var/lock/nova
 --allow_admin_api=true
 --use_deprecated_auth=false
---auth_strategy=keystone
---scheduler_driver=nova.scheduler.simple.SimpleScheduler
+--connection_type=libvirt
+--root_helper=sudo nova-rootwrap
+
+# API
 --s3_host=$host_ip_entry
 --ec2_host=$host_ip_entry
---rabbit_host=$host_ip_entry
 --cc_host=$host_ip_entry
 --nova_url=http://$host_ip_entry:8774/v1.1/
---routing_source_ip=$host_ip_entry
---glance_api_servers=$host_ip_entry:9292
---image_service=nova.image.glance.GlanceImageService
---iscsi_ip_prefix=192.168.22
---sql_connection=mysql://nova:$password@$host_ip_entry/nova
 --ec2_url=http://$host_ip_entry:8773/services/Cloud
 --keystone_ec2_url=http://$host_ip_entry:5000/v2.0/ec2tokens
 --api_paste_config=/etc/nova/api-paste.ini
---libvirt_type=qemu
+
+
+# Rabbit
+--rabbit_host=$host_ip_entry
+
+# Glance
+--glance_api_servers=$host_ip_entry:9292
+--image_service=nova.image.glance.GlanceImageService
+
+
+# Compute
+--libvirt_type=kvm
 --libvirt_use_virtio_for_bridges=true
 --start_guests_on_host_boot=true
 --resume_guests_state_on_host_boot=true
---vnc_enabled=true
---vncproxy_url=http://$host_ip_con:6080
---vnc_console_proxy_url=http://$host_ip_con:6080
-# network specific settings
+
+# Console
+--novnc_enabled=true
+--novncproxy_base_url= http://$host_ip_entry:6080/vnc_auto.html
+--vncserver_proxyclient_address=$host_ip_local
+--vncserver_listen=$host_ip_local
+
+# Network
 --network_manager=nova.network.manager.FlatDHCPManager
+--dhcpbridge_flagfile=/etc/nova/nova.conf
+--dhcpbridge=/usr/bin/nova-dhcpbridge
 --public_interface=eth0
 --flat_interface=eth0
 --flat_network_bridge=br100
 --fixed_range=$fixed_range
---floating_range=$floating_range
---network_size=$floating_size
---flat_network_dhcp_start=$fixed_start
 --flat_injected=False
---force_dhcp_release
---iscsi_helper=tgtadm
---connection_type=libvirt
---root_helper=sudo nova-rootwrap
---verbose=False
+--force_dhcp_release=True
+--multi_host=True
 " > /etc/nova/nova.conf
 
 # sync db
